efb06b5bbd5ccb444d5b7681b914161e37823819
Eduardo Junior <edujrrib@gmail.com>
2016-08-10 23:42:34 -0300

    Remove verificações no script bash

--- _deploy.sh
+++ _deploy.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-set -e
-
-[ -z "${GITHUB_PAT}" ] && exit 0
-[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
-
 git config --global user.email "edujrrib@gmail.com"
 git config --global user.name "Travis boot"
 
